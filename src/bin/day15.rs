1294c1573ff881aad37a28bad2f3b6dc6e1fca3d
Lanqing Huang <lqhuang@outlook.com>
2021-01-10 18:27:14 +0800

    Improve performance for day 15

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -10,13 +10,13 @@
 //!
 //! In this game, the players take turns saying **numbers**. They begin by
 //! taking turns reading from a list of **starting numbers** (your puzzle
-//! input). Then, each turn consists of considering the **most recently spoken number**:
+//! input). Then, each turn consists of considering the **most recently spoken
+//! number**:
 //!
 //! - If that was the **first** time the number has been spoken, the current
 //!   player says `0`.
-//! - Otherwise, the number had been spoken before; the current player
-//!   announces **how many turns apart** the number is from when it was
-//!   previously spoken.
+//! - Otherwise, the number had been spoken before; the current player announces
+//!   **how many turns apart** the number is from when it was previously spoken.
 //!
 //! So, after the starting numbers, each turn results in that player speaking
 //! aloud either `0` (if the last number is new) or an **age** (if the last
@@ -27,15 +27,15 @@
 //! - **Turn 1**: The 1st number spoken is a starting number, `0`.
 //! - **Turn 2**: The 2nd number spoken is a starting number, `3`.
 //! - **Turn 3**: The 3rd number spoken is a starting number, `6`.
-//! - **Turn 4**: Now, consider the last number spoken, `6`. Since that was
-//!   the first time the number had been spoken, the 4th number spoken is `0`.
-//! - **Turn 5**: Next, again consider the last number spoken, `0`. Since it
-//!   had been spoken before, the next number to speak is the difference
-//!   between the turn number when it was last spoken (the previous turn, `4`)
-//!   and the turn number of the time it was most recently spoken before then
-//!   (turn `1`). Thus, the 5th number spoken is `4 - 1`, `3`.
-//! - **Turn 6**: The last number spoken, `3` had also been spoken before,
-//!   most recently on turns `5` and `2`. So, the 6th number spoken is `5 - 2`, `3`.
+//! - **Turn 4**: Now, consider the last number spoken, `6`. Since that was the
+//!   first time the number had been spoken, the 4th number spoken is `0`.
+//! - **Turn 5**: Next, again consider the last number spoken, `0`. Since it had
+//!   been spoken before, the next number to speak is the difference between the
+//!   turn number when it was last spoken (the previous turn, `4`) and the turn
+//!   number of the time it was most recently spoken before then (turn `1`).
+//!   Thus, the 5th number spoken is `4 - 1`, `3`.
+//! - **Turn 6**: The last number spoken, `3` had also been spoken before, most
+//!   recently on turns `5` and `2`. So, the 6th number spoken is `5 - 2`, `3`.
 //! - **Turn 7**: Since `3` was just spoken twice in a row, and the last two
 //!   turns are `1` turn apart, the 7th number spoken is `1`.
 //! - **Turn 8**: Since `1` is new, the 8th number spoken is `0`.
@@ -74,10 +74,16 @@
 //! - Given `3,1,2`, the `30000000th` number spoken is `362`.
 //!
 //! Given your starting numbers, **what will be the 30000000th number spoken?**
-use std::env;
-use std::fs;
+#![feature(test)]
+extern crate test;
 
 use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::mem::replace;
+use std::time::Instant;
+
+use test::Bencher;
 
 fn parse_input(input: &str) -> Vec<usize> {
     input
@@ -86,39 +92,40 @@ fn parse_input(input: &str) -> Vec<usize> {
         .collect()
 }
 
-fn mem_game_solver(starting_nums: &[usize], nth: usize) -> usize {
-    let start_len = starting_nums.len();
-    let idx = if nth < 1 {
-        panic!("Nth should be greater and equal to 1.")
-    } else {
-        nth - 1
-    };
+/// Good idea:
+/// https://github.com/jhenninger/advent-of-code-2020/blob/master/day15/src/main.rs
+fn _game_solver_jhenninger(starting_nums: &[usize], target: usize) -> usize {
+    let mut start = starting_nums.iter().cloned();
+    let mut seen = HashMap::new();
+    let mut prev = None;
 
-    if idx < start_len {
-        starting_nums[nth]
-    } else {
-        let mut dict = starting_nums
-            .iter()
-            .enumerate()
-            .map(|(i, val)| (*val, i))
-            .take(start_len - 1)
-            .collect::<HashMap<usize, usize>>();
-        let mut prev = *starting_nums.last().unwrap();
-        let mut curr: usize = 0;
-
-        for i in start_len..nth {
-            if dict.contains_key(&prev) {
-                curr = i - 1 - *dict.get(&prev).unwrap();
-            } else {
-                curr = 0;
-            }
-            dict.insert(prev, i - 1);
-            prev = curr;
-            // println!("{} val: {}", i, curr);
-        }
-
-        curr
+    for turn in 0..target {
+        let number = prev.map(|p| turn - seen.insert(p, turn).unwrap_or(turn));
+        prev = start.next().or(number);
     }
+
+    prev.unwrap()
+}
+
+fn game_solver(starting_nums: &[usize], nth: usize) -> usize {
+    let arr_size = nth.max(*starting_nums.iter().max().unwrap() + 1);
+    let mut spoken: Vec<usize> = vec![0; arr_size];
+    let mut start = starting_nums.iter().cloned();
+    let mut prev = None;
+
+    for idx in 0..nth {
+        let number = prev.map(|p| {
+            let last = replace(&mut spoken[p], idx);
+
+            idx - match last {
+                0 => idx,
+                _ => last,
+            }
+        });
+        prev = start.next().or(number);
+    }
+
+    prev.unwrap()
 }
 
 fn main() -> Result<(), &'static str> {
@@ -132,11 +139,16 @@ fn main() -> Result<(), &'static str> {
 
     let starting_nums = parse_input(&input);
 
-    let num1 = mem_game_solver(&starting_nums, 2020);
+    let num1 = game_solver(&starting_nums, 2020);
     println!("The 2020th number spoken is {}", num1);
 
-    let num2 = mem_game_solver(&starting_nums, 30000000);
-    println!("The 30000000th number spoken is {}", num2);
+    let start = Instant::now();
+    let num2 = game_solver(&starting_nums, 30000000);
+    let duration = start.elapsed();
+    println!(
+        "The 30000000th number spoken is {}. Time elapsed is {:?}.",
+        num2, duration,
+    );
 
     Ok(())
 }
@@ -150,15 +162,15 @@ mod tests {
         let nth = 2020;
 
         let starting_nums = [0, 3, 6];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
+        let spoken_num = game_solver(&starting_nums, nth);
         assert_eq!(spoken_num, 436);
 
         let starting_nums = [1, 2, 3];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
+        let spoken_num = game_solver(&starting_nums, nth);
         assert_eq!(spoken_num, 27);
 
         let starting_nums = [3, 1, 2];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
+        let spoken_num = game_solver(&starting_nums, nth);
         assert_eq!(spoken_num, 1836);
     }
 
@@ -167,18 +179,37 @@ mod tests {
         let nth = 30000000;
 
         let starting_nums = [0, 3, 6];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
+        let spoken_num = game_solver(&starting_nums, nth);
         assert_eq!(spoken_num, 175594);
 
         let starting_nums = [1, 2, 3];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
+        let spoken_num = game_solver(&starting_nums, nth);
         assert_eq!(spoken_num, 261214);
-
-        let starting_nums = [3, 1, 2];
-        let spoken_num = mem_game_solver(&starting_nums, nth);
-        assert_eq!(spoken_num, 362);
     }
 
-    // #[bench]
-    // fn bench_mem_game(b: &mut Bencher) {}
+    #[test]
+    fn test_array_size() {
+        let nth = 10;
+        let starting_nums = [12, 1, 16, 3, 11, 0];
+        let spoken_num = game_solver(&starting_nums, nth);
+        assert_eq!(spoken_num, 0);
+    }
+
+    #[bench]
+    fn bench_mem_game(b: &mut Bencher) {
+        let nth = 2020;
+        let starting_nums = [0, 3, 6];
+        b.iter(|| {
+            game_solver(&starting_nums, nth);
+        });
+    }
+
+    #[bench]
+    fn bench_mem_game_large_nth(b: &mut Bencher) {
+        let nth = 30000000;
+        let starting_nums = [0, 3, 6];
+        b.iter(|| {
+            game_solver(&starting_nums, nth);
+        });
+    }
 }
